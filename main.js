8a5d533ee9ed065a4f50577903dafc151d12e8d9
banikathuria <79353266+banikathuria@users.noreply.github.com>
2021-12-01 19:22:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+canvas= document.getElementById("myCanvas");
+ctx=canvas.getContext("2d")
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown" , my_keydown)
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+	    aplhabetkey();
+	else{
+		otherkey();
+		
+	}
+	
+	if(keyPressed >=48 && keyPressed<=57)
+	numberkey();
+else{
+	otherkey();
+	
+}
+if(keyPressed >=37 && keyPressed<=40)
+	arrowkey();
+else{
+	otherkey();
+	
+}
+if(keyPressed >=17 && keyPressed<=27)
+	numberkey();
+else{
+	otherkey();
+	
+}
+
+}
+
+
+function aplhabetkey()
+{
+	img_image ="Alpkey.png"
+	add()
+
+}
+function numberkey()
+{
+	img_image ="numkey.png"
+	add()
+}
+function arrowkey()
+{
+	img_image ="Arrkey.png"
+	add()
+}
+function specialkey()
+{
+	img_image ="spkey.png"
+	add()
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
